f1a5e5b3a191c4c05f334832957538f4d1153825
tumikole <ramollosamuel2@gmail.com>
2021-08-03 15:59:53 +0200

    adding html, javascript and css files

---
+++ main.js
@@ -0,0 +1,32 @@
+// const submit = document.querySelector('#subBtn')
+// const form = document.querySelector('#myForm')
+// lastname.addEventListener('click', updateSurname);
+
+// submit.addEventListener('click', updateName);
+
+// function updateName() {
+//     const firstname = document.querySelector('#fname');
+//     const lastname = document.querySelector('#lname');
+
+//   let name = prompt('Enter a new name');
+//   firstname.value = name;{
+//       alert("Name changed successfully", name);
+//   }
+  
+//   let surname = prompt('Enter a new surname');
+//   lastname.value = surname;{
+//     alert("Surname changed successfully", surname);
+// }
+
+//   }
+
+// const firstname = document.querySelector('#fname')
+// const lastname = document.querySelector('#lname')
+// const email = document.querySelector('#email')
+// const contact = document.querySelector('#contact')
+// const salary = document.querySelector('#salary')
+// const position = document.querySelector('#position')
+
+
+
+
